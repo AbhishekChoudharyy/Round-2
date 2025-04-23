e0199057b2cf32fdf4f68e22650135f91cfc3f57
Abhishek Choudhary <142403448+AbhishekChoudharyy@users.noreply.github.com>
2025-04-24 02:31:58 +0530

    remove unused React import statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import Navbar from './components/Navbar';
 import Landingpage from './components/Landingpage';
 import ScrollVelocity from './components/ScrollVelocity';
